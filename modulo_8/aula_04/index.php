1de91e9a67be16d64d231dce8318721bbe2b4fcf
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-02-03 20:13:12 -0300

    delete

---
+++ modulo_8/aula_04/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $pdo = new PDO('mysql:host=localhost;dbname=modulo_8','root','681015');
+
+    $id = 3;
+
+    //OR ou AND
+
+    $sql = $pdo->prepare("DELETE FROM `clientes` WHERE id=?");
+
+    if ($sql->execute(array($id))) {
+        echo 'Meu cliente foi deletado com sucesso!';
+    }
+
+?>
