4cf587bb63f945bab975a970c14b4e5b0d88b4f9
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-02-03 21:43:56 -0300

    LEFT, RIGHT JOIN

---
+++ modulo_8/aula_07/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $pdo = new PDO('mysql:host=localhost;dbname=db_teste','root','681015');
+   // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = $pdo->prepare("SELECT * FROM `clientes` RIGHT JOIN cargos ON `clientes`.`cargo` = `cargos`.`id`");
+    $sql->execute();
+    $clientes = $sql->fetchAll();
+    foreach ($clientes as $key => $value){
+        echo $value['nome'];
+        echo ' | ';
+        echo $value['nome_cargo'];
+        echo '<hr>';
+    }
+
+
+
+
+?>
