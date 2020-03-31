baf6a7a24a7524bb93d6f7982b8f9cbe5352e20f
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-03-31 12:52:47 -0300

    Group By

--- cursoMySql/cadastro.sql
+++ cursoMySql/cadastro.sql
@@ -266,6 +266,47 @@ SELECT min(peso) FROM gafanhotos WHERE sexo = 'F' AND nacionalidade <> 'Brasil' 
 SELECT count(*) FROM gafanhotos WHERE sexo = 'F' AND altura > 1.90;
 
 
+/*Aula 13*/
+/*GROUP BY*/
+SELECT  totaulas FROM cursos 
+GROUP BY totaulas ORDER BY totaulas; /*Agrupa os dados de mesmo valor*/
+
+SELECT  totaulas,count(*) FROM cursos 
+GROUP BY totaulas ORDER BY totaulas; /*Agrupa os dados de mesmo valor e mostra a quantidade de cada agrupamento*/
+
+SELECT carga FROM cursos 
+WHERE totaulas > 20 GROUP BY carga; /*Mostra e agrupa todos os curso onde a carga é maior do que 20*/
+
+SELECT carga, count(*) FROM cursos 
+WHERE totaulas > 20 GROUP BY carga; /*Mostra e agrupa todos os curso onde a carga é maior do que 20 e os conta*/
+
+
+SELECT ano, count(*) FROM cursos G
+ROUP BY ano ORDER BY count(*);/*Agrupa os cursos por ano, conta quantos tem de cada ano e ordena por quantidade*/
+
+SELECT ano, count(*) FROM cursos 
+GROUP BY ano HAVING count(ano) >= 3; ORDER BY count(*);/*Agrupa os cursos por ano, conta quantos tem de cada ano e ordena por quantidade mostrando somente os que tem quantidade maior ou igual a 3*/
+
+SELECT avg(carga) FROM cursos; /*Mostra a média da carga de todos os cursos*/
+
+SELECT  carga, count(*) FROM cursos 
+WHERE ano > 2013 GROUP BY carga HAVING carga > (SELECT avg(carga) FROM cursos);
+/*Seleciona  a carga e total dela, filtra somente as acima de 2013 agrupa por carga e mostra somente os que tem a carga acima da média*/
+
+
+/*1- Lista com as profissões dos gafanhotos e seus respectivos quantitativos*/
+SELECT profissao, count(*) FROM gafanhotos group by profissao;
+
+SELECT sexo, count(*) FROM gafanhotos WHERE nascimento > '2005-01-01' group by sexo ;
+
+SELECT nacionalidade, count(*) FROM gafanhotos
+WHERE nacionalidade != 'Brasil' 
+GROUP BY nacionalidade having count(nacionalidade) > 3;
+
+SELECT altura, count(*) FROM gafanhotos
+WHERE peso > '100' AND altura > (SELECT avg(altura) FROM gafanhotos) group by altura;
+
+
 
 /*
 Comandos DDL: 
